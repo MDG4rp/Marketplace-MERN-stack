100132d391b77b91acb0d7e0c847f364d67776cb
Giacomo <ggiraldi18@gmail.com>
2024-08-27 11:36:36 +0200

    fixed the not-storing-jwt issue after refreshing the browser

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,20 +1,30 @@
 import axios from "axios";
+import Cookies from "js-cookie";
+
+const api = import.meta.env.VITE_API_URL;
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: api,
   headers: {
     "Content-Type": "application/json",
   },
+  withCredentials: true,
 });
 
 axiosInstance.interceptors.request.use(
   function (config) {
+    const authToken = Cookies.get('_auth');
+    const authType = Cookies.get('_auth_type');
+    if (authToken && authType) {
+      config.headers['Authorization'] = `${authType} ${authToken}`;
+    }
     return config;
   },
   function (error) {
     return Promise.reject(error);
   }
 );
+
 axiosInstance.interceptors.response.use(
   function (response) {
     return response;
